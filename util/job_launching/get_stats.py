afab3f9bb5634f76784c53d2dc824722d9eaa384
tgrogers <timrogers@gmail.com>
2017-06-30 23:53:50 -0400

    Adding the ability to just get the latest stats for a given bench/config and not tied to any specific jobIds

--- util/job_launching/get_stats.py
+++ util/job_launching/get_stats.py
@@ -45,9 +45,12 @@ parser.add_option("-N", "--sim_name", dest="sim_name",
                        " give you the status of the latest run with that name."+ \
                        " if you want older runs from this name, then just point it directly at the"+\
                        " logfile with \"-l\"", default="")
-#parser.add_option("-y", "--configs_yml", dest="configs_yml", default="",
-#                  help="If this option is specified, then sim_name and logfile are ignored." +\
-#                       "Instead, the output files that will be parsed will")
+parser.add_option("-c", "--configs_yml", dest="configs_yml", default="",
+                  help="If this option is specified, then sim_name and logfile are ignored." +\
+                       "Instead, the output files that will be parsed will")
+parser.add_option("-a", "--apps_yml", dest="apps_yml", default="",
+                  help="If this option is specified, then sim_name and logfile are ignored." +\
+                       "Instead, the output files that will be parsed will")
 #parser.add_option("-b", "--simulator_build", dest="simulator_build", default="",
 #                  help="If you only want data from a particular build of the simulator, specify this flag.")
 parser.add_option("-s", "--stats_yml", dest="stats_yml", default="",
@@ -59,100 +62,116 @@ options.run_dir = options.run_dir.strip()
 options.sim_name = options.sim_name.strip()
 
 cuda_version = common.get_cuda_version()
-
-parsed_logfiles = []
-logfiles_directory = this_directory + "../job_launching/logfiles/"
-if options.logfile == "":
-    if not os.path.exists(logfiles_directory):
-        exit("No logfile specified and the default logfile directory cannot be found")
-    all_logfiles = [os.path.join(logfiles_directory, f) \
-                       for f in os.listdir(logfiles_directory) if(re.match(r'sim_log.*',f))]
-    if len(all_logfiles) == 0:
-        exit("ERROR - No Logfiles in " + logfiles_directory)
-    if options.sim_name != "":
-        named_sim = []
-        for logf in all_logfiles:
-            match_str = r".*\/sim_log\.{0}\..*".format( options.sim_name )
-            if re.match( match_str, logf ):
-                named_sim.append( logf )
-        if len( named_sim ) == 0:
-            exit( "Could not find logfiles for job with the name \"{0}\"".format( options.sim_name ) )
-        all_logfiles = named_sim
-    parsed_logfiles.append(max(all_logfiles, key=os.path.getmtime))
-elif options.logfile == "all":
-    parsed_logfiles = [os.path.join(logfiles_directory, f) \
-                    for f in os.listdir(logfiles_directory) if(re.match(r'sim_log.*\.latest',f))]
-else:
-    parsed_logfiles.append(common.file_option_test( options.logfile, "", this_directory ))
-
-print "Using logfiles " + str(parsed_logfiles)
-
 options.run_dir = common.dir_option_test( options.run_dir, this_directory + ("../../sim_run_%s/"%cuda_version),
                                           this_directory )
+if not os.path.isdir(options.run_dir):
+    exit(options.run_dir + " does not exist - specify the run directory where the benchmark/config dirs exist")
+
 options.stats_yml = common.file_option_test( options.stats_yml, os.path.join( this_directory, "stats", "example_stats.yml" ),
                                             this_directory )
+options.configs_yml = common.file_option_test( options.configs_yml, "", this_directory )
+options.apps_yml = common.file_option_test( options.apps_yml, "", this_directory )
+
+stat_map = {}
+configs = set()
+apps_and_args = set()
+specific_jobIds = {}
 
 stats_to_pull = {}
 stats_yaml = yaml.load(open(options.stats_yml))
 stats= {}
 for stat in stats_yaml['collect']:
-    stats_to_pull[stat] = re.compile(stats_yaml['collect'][stat])#.decode('string_escape')
+    stats_to_pull[stat] = re.compile(stats_yaml['collect'][stat])
 
-if not os.path.isdir(options.run_dir):
-    exit(options.run_dir + " does not exist - specify the run directory where the benchmark/config dirs exist")
+if options.configs_yml != "" and options.apps_yml != "":
+    for app in common.parse_app_yml( options.apps_yml ):
+        a,b,exe_name,args_list = app
+        for args in args_list:
+            apps_and_args.add( os.path.join(exe_name, re.sub(r"[^a-z^A-Z^0-9]", "_", args.strip())) )
+    for config, params, gpuconf_file in common.parse_config_yml( options.configs_yml ):
+        configs.add( config )
+else:
+    # This code gets the logfiles to pull the stats from if you are using the "-l" or "-N" option
+    parsed_logfiles = []
+    logfiles_directory = this_directory + "../job_launching/logfiles/"
+    if options.logfile == "":
+        if not os.path.exists(logfiles_directory):
+            exit("No logfile specified and the default logfile directory cannot be found")
+        all_logfiles = [os.path.join(logfiles_directory, f) \
+                           for f in os.listdir(logfiles_directory) if(re.match(r'sim_log.*',f))]
+        if len(all_logfiles) == 0:
+            exit("ERROR - No Logfiles in " + logfiles_directory)
+        if options.sim_name != "":
+            named_sim = []
+            for logf in all_logfiles:
+                match_str = r".*\/sim_log\.{0}\..*".format( options.sim_name )
+                if re.match( match_str, logf ):
+                    named_sim.append( logf )
+            if len( named_sim ) == 0:
+                exit( "Could not find logfiles for job with the name \"{0}\"".format( options.sim_name ) )
+            all_logfiles = named_sim
+        parsed_logfiles.append(max(all_logfiles, key=os.path.getmtime))
+    elif options.logfile == "all":
+        parsed_logfiles = [os.path.join(logfiles_directory, f) \
+                        for f in os.listdir(logfiles_directory) if(re.match(r'sim_log.*\.latest',f))]
+    else:
+        parsed_logfiles.append(common.file_option_test( options.logfile, "", this_directory ))
 
-stat_map = {}
-configs = set()
-benchmark_args = set()
+    print "Using logfiles " + str(parsed_logfiles)
 
-# At this point we have the logfile we want to get a synopsis for.
-for logfile in parsed_logfiles:
-    if not os.path.isfile(logfile):
-        exit("Cannot open Logfile " + logfile)
+    for logfile in parsed_logfiles:
+        if not os.path.isfile(logfile):
+            exit("Cannot open Logfile " + logfile)
 
-    # Create the output file
-    base_logname = os.path.basename(logfile)
+        with open( logfile ) as f:
+            for line in f:
+                time, jobId, app ,args, config, jobname = line.split()
+                configs.add(config)
+                app_and_args = os.path.join( app, args )
+                apps_and_args.add( app_and_args )
+                specific_jobIds[ config + app_and_args ] = jobId
 
-    # Parse the logfile for job ids
-    with open( logfile ) as f:
-        for line in f:
-            time, jobId, app ,args, config, jobname = line.split()
-            configs.add(config)
-            benchmark_args.add(app + args)
+for app_and_args in apps_and_args:
+    for config in configs:
+        # now get the right output file
+        output_dir = os.path.join(options.run_dir, app_and_args, config)
+        if not os.path.isdir( output_dir ):
+            print("WARNING the outputdir " + output_dir + " does not exist")
+            continue
+        
+        if config + app_and_args in specific_jobIds:
+            jobId = specific_jobIds[ config + app_and_args ]
+            outfile = os.path.join(output_dir, app_and_args.replace("/", "-") + "." + "o" + jobId)
+        else:
+            all_outfiles = [os.path.join(output_dir, f) \
+                           for f in os.listdir(output_dir) if(re.match(r'.*\.o[0-9]+',f))]
+            outfile = max(all_outfiles, key=os.path.getmtime)
 
-            # now get the right logfile
-            output_dir = os.path.join(options.run_dir, app, args, config)
-            if not os.path.isdir( output_dir ):
-                print("WARNING the outputdir " + output_dir + " does not exist")
-                continue
+        stat_found = set()
 
-            outfile = os.path.join(output_dir, app + "-" + args + "." + "o" + jobId)
+        if not os.path.isfile( outfile ):
+            print "WARNING - " + outfile + " does not exist"
+            continue
 
-            stat_found = set()
+        # Only go up for 1000 lines looking for stuff
+        MAX_LINES = 100000
+        count = 0
+        for line in reversed(open(outfile).readlines()):
+            count += 1
+            if count >= MAX_LINES:
+                break
 
-            if not os.path.isfile( outfile ):
-                print "WARNING - " + outfile + " does not exist"
-                continue
-
-            # Only go up for 1000 lines looking for stuff
-            MAX_LINES = 100000
-            count = 0
-            for line in reversed(open(outfile).readlines()):
-                count += 1
-                if count >= MAX_LINES:
-                    break
-
-                # pull out some stats
-                for stat_name, token in stats_to_pull.iteritems():
-                    if stat_name in stat_found:
-                        continue
-                    existance_test = token.search( line.rstrip() )
-                    if existance_test != None:
-                        stat_found.add(stat_name)
-                        number = existance_test.group(1).strip()
-                        stat_map[app + args + config + stat_name] = number
-                if len(stat_found) == len(stats_to_pull):
-                    break
+            # pull out some stats
+            for stat_name, token in stats_to_pull.iteritems():
+                if stat_name in stat_found:
+                    continue
+                existance_test = token.search( line.rstrip() )
+                if existance_test != None:
+                    stat_found.add(stat_name)
+                    number = existance_test.group(1).strip()
+                    stat_map[app_and_args + config + stat_name] = number
+            if len(stat_found) == len(stats_to_pull):
+                break
 
 # After collection, spew out the tables
 DIVISION = "-" * 100
@@ -163,7 +182,7 @@ for stat_name, token in stats_to_pull.iteritems():
     for config in configs:
         csv_str += config + ","
     csv_str += "\n"
-    for appargs in benchmark_args:
+    for appargs in apps_and_args:
         csv_str += appargs + ","
         for config in configs:
             if appargs + config + stat_name in stat_map:
