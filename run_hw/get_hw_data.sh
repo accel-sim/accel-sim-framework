327ad10a4200ce59943c929831afa97af66db6d7
tgrogers <tgrogers@purdue.edu>
2018-03-18 01:21:23 -0400

    Adding in P100 data

--- run_hw/get_hw_data.sh
+++ run_hw/get_hw_data.sh
@@ -1,5 +1,6 @@
 export DATA_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 TITANX_PASCAL_DATA=$DATA_ROOT/TITAN-X-PASCAL
+P100_PASCAL_DATA=$DATA_ROOT/TESLA-P100
 
 if [ ! -d $TITANX_PASCAL_DATA ]; then
     echo "in"
@@ -8,5 +9,9 @@ if [ ! -d $TITANX_PASCAL_DATA ]; then
     rm pascal.titanx.cycle.tgz
 fi
 
-
-
+if [ ! -d $P100_PASCAL_DATA ]; then
+    echo "in"
+    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/pascal.tesla.p100.cycles.tgz .
+    tar -xzvf pascal.tesla.p100.cycles.tgz -C $DATA_ROOT
+    rm pascal.tesla.p100.cycles.tgz
+fi
