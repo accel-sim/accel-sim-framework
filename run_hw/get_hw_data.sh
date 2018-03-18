3d1f15ff2a75d59b610495c7710ba2b1e7232da1
tgrogers <tgrogers@purdue.edu>
2018-03-18 01:55:25 -0400

    Fixing the wget

--- run_hw/get_hw_data.sh
+++ run_hw/get_hw_data.sh
@@ -4,14 +4,14 @@ P100_PASCAL_DATA=$DATA_ROOT/TESLA-P100
 
 if [ ! -d $TITANX_PASCAL_DATA ]; then
     echo "in"
-    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/pascal.titanx.cycle.tgz .
+    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/pascal.titanx.cycle.tgz
     tar -xzvf pascal.titanx.cycle.tgz -C $DATA_ROOT
     rm pascal.titanx.cycle.tgz
 fi
 
 if [ ! -d $P100_PASCAL_DATA ]; then
     echo "in"
-    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/pascal.tesla.p100.cycles.tgz .
+    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/pascal.tesla.p100.cycles.tgz
     tar -xzvf pascal.tesla.p100.cycles.tgz -C $DATA_ROOT
     rm pascal.tesla.p100.cycles.tgz
 fi
