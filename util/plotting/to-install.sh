05abd23ea5bd8ea1ff4c579676c60da3f0a4f7ef
tgrogers <tgrogers@purdue.edu>
2018-04-01 12:05:26 -0400

    One extra thing you need to do is point python to your custom path if you haven't already set this up somewhere

--- util/plotting/to-install.sh
+++ util/plotting/to-install.sh
@@ -1,2 +1,3 @@
 pip install --target=/home/tgrogers-raid/a/tgrogers/python-package/ pandas
 pip install --target=/home/tgrogers-raid/a/tgrogers/python-package/ plotly
+export PYTHONPATH=/home/tgrogers-raid/a/tgrogers/python-package/:$PYTHONPATH
