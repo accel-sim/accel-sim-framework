80adaff43ae615f84184b70ea06b3c6a5bc3499a
tgrogers <timrogers@purdue.edu>
2018-05-05 00:44:09 -0400

    Some timing information on how long it takes to run all this stuff

--- util/correlation/lrrr.collection.sh
+++ util/correlation/lrrr.collection.sh
@@ -1,3 +1,4 @@
+start=`date +%s`
 DATE=`date '+%Y-%m-%d--%H.%M.%S'`
 ./run_hw.py -D 0 -c -B `cat ../job_launching/apps/all-apps.list`
 cp -r ../../run_hw/device-0 ../../run_hw/TESLA-P100
@@ -5,3 +6,6 @@ cd ../../run_hw
 tar zcvf pascal.tesla.p100.cycles.tgz ./TESLA-P100
 ssh dynamo.ecn.purdue.edu mv /home/dynamo/a/tgrogers/website/gpgpu-sim/hw_data/pascal.tesla.p100.cycles.tgz /home/dynamo/a/tgrogers/website/gpgpu-sim/hw_data/bak.$DATE.pascal.tesla.p100.cycles.tgz
 scp pascal.tesla.p100.cycles.tgz dynamo.ecn.purdue.edu:~/website/gpgpu-sim/hw_data/
+end=`date +%s`
+runtime=$((end-start))
+echo "Runtime: $runtime"
