ad31367906b8d221e43da988e1a3c99cea741960
tgrogers <timrogers@purdue.edu>
2018-05-05 00:44:09 -0400

    Some timing information on how long it takes to run all this stuff

--- util/correlation/tgrogers-pc01.collection.sh
+++ util/correlation/tgrogers-pc01.collection.sh
@@ -1,3 +1,4 @@
+start=`date +%s`
 DATE=`date '+%Y-%m-%d--%H.%M.%S'`
 ./run_hw.py -D 0 -B `cat ../job_launching/apps/all-apps.list`
 cp -r ../../run_hw/device-0 ../../run_hw/TITAN-X-PASCAL
@@ -5,3 +6,6 @@ cd ../../run_hw
 tar zcvf pascal.titanx.cycle.tgz ./TITAN-X-PASCAL
 ssh dynamo.ecn.purdue.edu mv /home/dynamo/a/tgrogers/website/gpgpu-sim/hw_data/pascal.titanx.cycle.tgz /home/dynamo/a/tgrogers/website/gpgpu-sim/hw_data/bak.$DATE.pascal.titanx.cycle.tgz
 scp pascal.titanx.cycle.tgz dynamo.ecn.purdue.edu:~/website/gpgpu-sim/hw_data/
+end=`date +%s`
+runtime=$((end-start))
+echo "Runtime: $runtime"
