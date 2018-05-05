ce44170a6d99f3c6eb9ba6f4a0cb709ca2b8bcec
tgrogers <timrogers@purdue.edu>
2018-05-05 00:50:09 -0400

    Adding the timing to the pc11 script

--- util/correlation/aamodt-pc11.collection.sh
+++ util/correlation/aamodt-pc11.collection.sh
@@ -1,3 +1,4 @@
+start=`date +%s`
 DATE=`date '+%Y-%m-%d--%H.%M.%S'`
 ./run_hw.py -D 0 -B `cat ../job_launching/apps/all-apps.list` -R 5
 cp -r ../../run_hw/device-0 ../../run_hw/1080TI_PASCAL
@@ -5,3 +6,6 @@ cd ../../run_hw
 tar zcvf pascal.1080ti.cycle.tgz ./1080TI_PASCAL
 ssh dynamo.ecn.purdue.edu mv /home/dynamo/a/tgrogers/website/gpgpu-sim/hw_data/pascal.1080ti.cycle.tgz /home/dynamo/a/tgrogers/website/gpgpu-sim/hw_data/bak.$DATE.pascal.1080ti.cycle.tgz
 scp pascal.1080ti.cycle.tgz dynamo.ecn.purdue.edu:~/website/gpgpu-sim/hw_data/
+end=`date +%s`
+runtime=$((end-start))
+echo "Runtime: $runtime"
