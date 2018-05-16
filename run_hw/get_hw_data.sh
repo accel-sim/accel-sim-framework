fd6279d4d29735a3d749b5f1f3a7f429bc5a3ab7
Tim Rogers <timrogers@purdue.edu>
2018-05-16 17:59:24 -0400

    Adding in the colta HW data

--- run_hw/get_hw_data.sh
+++ run_hw/get_hw_data.sh
@@ -3,31 +3,34 @@ TITANX_PASCAL_DATA=$DATA_ROOT/TITAN-X-PASCAL
 P100_PASCAL_DATA=$DATA_ROOT/TESLA-P100
 FERMI_GTX480_DATA=$DATA_ROOT/GTX480
 1080Ti_DATA=$DATA_ROOT/1080Ti
+TITANV_DATA=$DATA_ROOT/TITANV
 
 if [ ! -d $TITANX_PASCAL_DATA ]; then
-    echo "in"
     wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/pascal.titanx.cycle.tgz
     tar -xzvf pascal.titanx.cycle.tgz -C $DATA_ROOT
     rm pascal.titanx.cycle.tgz
 fi
 
 if [ ! -d $P100_PASCAL_DATA ]; then
-    echo "in"
     wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/pascal.tesla.p100.cycles.tgz
     tar -xzvf pascal.tesla.p100.cycles.tgz -C $DATA_ROOT
     rm pascal.tesla.p100.cycles.tgz
 fi
 
 if [ ! -d $FERMI_GTX480_DATA ]; then
-    echo "in"
     wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/fermi.gtx480.cycle.tgz
     tar -xzvf fermi.gtx480.cycle.tgz -C $DATA_ROOT
     rm fermi.gtx480.cycle.tgz
 fi
 
 if [ ! -d $1080Ti_DATA ]; then
-    echo "in"
     wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/pascal.1080ti.cycle.tgz
     tar -xzvf pascal.1080ti.cycle.tgz -C $DATA_ROOT
     rm pascal.1080ti.cycle.tgz
+fi
+
+if [ ! -d $TITANV_DATA ]; then
+    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/volta.titanv.tgz
+    tar -xzvf volta.titanv.tgz -C $DATA_ROOT
+    rm volta.titanv.tgz
 fi
