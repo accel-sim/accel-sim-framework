1c5044fb5ee04ce40daa2d4bb08df633f388016b
tgrogers <tgrogers@purdue.edu>
2018-09-23 16:28:04 -0400

    Letting calling code specify which apps get corellated

--- util/plotting/correlate_and_publish.sh
+++ util/plotting/correlate_and_publish.sh
@@ -5,18 +5,18 @@ CORREL_THIS_DIR="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 EXTERNAL_PUBLISH_LOCATION="/home/dynamo/a/tgrogers/website/gpgpu-sim-plots/"
 EXTERNAL_SERVER="tgrogers@dynamo.ecn.purdue.edu"
 
-if [ $# -lt 1 ] ; then
-    echo "Err - Pass 1 at least one arguemnt, which is the configurations list."
+if [ $# -lt 2 ] ; then
+    echo "Err - Pass 2 at least one arguemnts, which are the configurations list and the apps to get"
     exit 1
 fi
 
-$CORREL_THIS_DIR/../job_launching/get_stats.py -R -K -k -B `cat $CORREL_THIS_DIR/../job_launching/apps/all-apps.list` -C $1 > stats-$$.csv
+$CORREL_THIS_DIR/../job_launching/get_stats.py -R -K -k -B $2 -C $1 > stats-$$.csv
 $CORREL_THIS_DIR/../../run_hw/get_hw_data.sh && rm -rf $CORREL_THIS_DIR/correl-html
 $CORREL_THIS_DIR/plot-correlation.py -c stats-$$.csv -H $CORREL_THIS_DIR/../../run_hw/
 
-if [ $# -eq 3 ] ; then
-    PLOTDIR=$2
-    BUILDNUM=$3
+if [ $# -eq 4 ] ; then
+    PLOTDIR=$3
+    BUILDNUM=$4
     ssh $EXTERNAL_SERVER mkdir -p $EXTERNAL_PUBLISH_LOCATION/$PLOTDIR/$BUILDNUM
     scp  $CORREL_THIS_DIR/correl-html/* $EXTERNAL_SERVER:$EXTERNAL_PUBLISH_LOCATION/$PLOTDIR/$BUILDNUM
     ssh $EXTERNAL_SERVER "cd $EXTERNAL_PUBLISH_LOCATION/$PLOTDIR && ln -s -f $BUILDNUM latest"
