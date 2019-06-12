dfc2d9dbb4e3b70019ded50e47d622419315d8ba
tgrogers <tgrogers@purdue.edu>
2019-06-12 16:50:27 -0400

    Fixing some whitespace issues

--- util/plotting/plot-correlation.py
+++ util/plotting/plot-correlation.py
@@ -303,9 +303,9 @@ def parse_hw_csv(csv_file, hw_data, appargs, logger):
                         continue
                     if processedCycle:
                         count = 0
-			if kcount < len(kdata):
+                        if kcount < len(kdata):
                             logger.log("Warning - number of kernels in cycle file mismatches kernels in the stats file:\n{0}".format(csv_file))
-			    continue
+                            continue
                         for elem in row:
                             if header[count] not in kdata[kcount]:
                                 kdata[kcount][header[count]] = []
