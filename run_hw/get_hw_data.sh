073085d28777377207a353ae4d8114a4b801d081
Tim Rogers <timrogers@purdue.edu>
2019-11-21 09:57:10 -0500

    Adding in stuff to get/clean the Turing data

--- run_hw/get_hw_data.sh
+++ run_hw/get_hw_data.sh
@@ -6,6 +6,14 @@ TI1080_DATA=$DATA_ROOT/1080TI_PASCAL
 TITANV_DATA=$DATA_ROOT/TITANV
 QUADRO_V100_DATA=$DATA_ROOT/QUADRO-V100
 TESLA_V100_DATA=$DATA_ROOT/TESLA-V100
+TURING_RTX_DATA=$DATA_ROOT/TURING-RTX2060
+
+if [ ! -d $TURING_RTX_DATA ]; then
+    TARFILE=turing.rtx2060.cycle.tgz
+    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/$TARFILE
+    tar -xzvf $TARFILE -C $DATA_ROOT
+    rm $TARFILE
+fi
 
 if [ ! -d $TITANX_PASCAL_DATA ]; then
     wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/hw_data/pascal.titanx.cycle.tgz
