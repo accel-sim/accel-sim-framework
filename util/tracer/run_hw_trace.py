bd4a0a32c922ac8cf70bff75e5ba96d24650f5fb
Mahmoud <k.mkhairy@yahoo.com>
2020-03-09 13:06:24 -0400

    add tracer and checkpoint tool

--- util/tracer/run_hw_trace.py
+++ util/tracer/run_hw_trace.py
@@ -28,10 +28,7 @@ parser.add_option("-D", "--device_num", dest="device_num",
                  default="0")
 parser.add_option("-n", "--norun", dest="norun", action="store_true", 
                  help="Do not actually run the apps, just create the dir structure and launch files")
-parser.add_option("-c", "--cycle_only", dest="cycle_only", action="store_true",
-                 help="Just get Kernel Duration")
-parser.add_option("-R", "--repeat_cycle", dest="repeat_cycle", default=0,
-                 help="When running the cycle tests, do them this many times (good when DVFS is enabled)")
+
 (options, args) = parser.parse_args()
 
 if not any([os.path.isfile(os.path.join(p, "nvprof")) for p in os.getenv("PATH").split(os.pathsep)]):
@@ -48,7 +45,7 @@ day_string = now_time.strftime("%y.%m.%d-%A")
 time_string = now_time.strftime("%H:%M:%S")
 logfile = day_string + "--" + time_string + ".csv"
 
-nvbit_path = os.path.join(this_directory, "..", "..", "..", "nvbit")
+nvbit_path = os.path.join(this_directory, "tracer_tool")
 
 for bench in benchmarks:
     edir, ddir, exe, argslist = bench
@@ -71,28 +68,11 @@ for bench in benchmarks:
 
         sh_contents = ""
         
-        # if not options.cycle_only:
-        #     sh_contents += "\nexport CUDA_VERSION=\"" + cuda_version + "\"; export CUDA_VISIBLE_DEVICES=\"" + options.device_num + "\" ; " +\
-        #         "LD_PRELOAD=" + os.path.join(nvbit_path, "tools", "traceall", "traceall.so") + " " + os.path.join(this_directory, edir,exe) +\
-        #         " " + str(args) + " 2>&1 | tee " + os.path.join(this_run_dir,"output_trace.txt") + " "
+        sh_contents += "\nexport CUDA_VERSION=\"" + cuda_version + "\"; export CUDA_VISIBLE_DEVICES=\"" + options.device_num + "\" ; " +\
+            "LD_PRELOAD=" + os.path.join(nvbit_path, "tools", "traceall", "traceall.so") + " " + os.path.join(this_directory, edir,exe) +\
+            " " + str(args) + " ; " + os.path.join(nvbit_path, "tools", "traceall", "traces-processing", "post-traces-processing") + " " +\
+            os.path.join(this_run_dir, "traces", "kernelslist") + " ; rm " + os.path.join(this_run_dir, "traces") + "/*.trace "
 
-        # for i in range(int(options.repeat_cycle)):
-        #     sh_contents += "\nexport CUDA_VERSION=\"" + cuda_version + "\"; export CUDA_VISIBLE_DEVICES=\"" + options.device_num + "\" ; " +\
-        #         "LD_PRELOAD=" + os.path.join(nvbit_path, "tools", "traceall", "traceall.so") + " " + os.path.join(this_directory, edir,exe) +\
-        #         " " + str(args) + " 2>&1 | tee " + os.path.join(this_run_dir,"output_trace.txt")
-
-        if not options.cycle_only:
-            sh_contents += "\nexport CUDA_VERSION=\"" + cuda_version + "\"; export CUDA_VISIBLE_DEVICES=\"" + options.device_num + "\" ; " +\
-                "LD_PRELOAD=" + os.path.join(nvbit_path, "tools", "traceall", "traceall.so") + " " + os.path.join(this_directory, edir,exe) +\
-                " " + str(args) + " ; " + os.path.join(nvbit_path, "tools", "traceall", "traces-processing", "post-traces-processing") + " " +\
-                os.path.join(this_run_dir, "traces", "kernelslist") + " ; rm " + os.path.join(this_run_dir, "traces") + "/*.trace "
-
-        for i in range(int(options.repeat_cycle)):
-            sh_contents += "\nexport CUDA_VERSION=\"" + cuda_version + "\"; export CUDA_VISIBLE_DEVICES=\"" + options.device_num + "\" ; " +\
-                "LD_PRELOAD=" + os.path.join(nvbit_path, "tools", "traceall", "traceall.so") + " " + os.path.join(this_directory, edir,exe) +\
-                " " + str(args) + " ; " + os.path.join(nvbit_path, "tools", "traceall", "traces-processing", "post-traces-processing") + " " +\
-                os.path.join(this_run_dir, "traces", "kernelslist") + " ; rm " + os.path.join(this_run_dir, "traces") + "/*.trace"
-                
         print ("sh_contents: ", sh_contents)
         print ("this_run_dir: ", this_run_dir)
         print ("this_dir", this_directory)
@@ -107,5 +87,5 @@ for bench in benchmarks:
             print "Running {0}".format(exe)
 
             if subprocess.call(["bash", "run.sh"]) != 0:
-                print "Error invoking profiler on {0}".format(this_run_dir)
+                print "Error invoking nvbit on {0}".format(this_run_dir)
             os.chdir(saved_dir)
