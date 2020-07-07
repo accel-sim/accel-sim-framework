f07375fbf770dc610f187801370a86f445984e29
Tim Rogers <timrogers@purdue.edu>
2020-07-07 09:39:32 -0400

    check for cuda

--- travis.sh
+++ travis.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -n "$CUDA_INSTALL_PATH" ]; then
+	echo "ERROR ** Install CUDA Toolkit and set CUDA_INSTALL_PATH.";
+	return;
+fi
+
 export PATH=$CUDA_INSTALL_PATH/bin:$PATH;
 source ./gpu-simulator/setup_environment.sh
-make -C ./gpu-simulator"
+make -C ./gpu-simulator
