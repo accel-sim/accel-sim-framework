5620442999da11d2c0556321eb80b41de043832e
tgrogers <tgrogers@purdue.edu>
2021-02-19 16:27:38 -0500

    updating installer to the newest NVBit and getting rid some random nonsense in the script

--- util/tracer_nvbit/install_nvbit.sh
+++ util/tracer_nvbit/install_nvbit.sh
@@ -1,11 +1,8 @@
 export BASH_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
-DATA_SUBDIR="/data_dirs/"
-DATA_ROOT=$BASH_ROOT$DATA_SUBDIR
 
-if [ ! -d $DATA_ROOT ]; then
-    wget https://github.com/NVlabs/NVBit/releases/download/1.5/nvbit-Linux-x86_64-1.5.tar.bz2
-    tar -xf nvbit-Linux-x86_64-1.5.tar.bz2 -C $BASH_ROOT
-    rm nvbit-Linux-x86_64-1.5.tar.bz2
-fi
+rm -rf $BASH_ROOT/nvbit_release
+wget https://github.com/NVlabs/NVBit/releases/download/1.5.3/nvbit-Linux-x86_64-1.5.3.tar.bz2
+tar -xf nvbit-Linux-x86_64-1.5.3.tar.bz2 -C $BASH_ROOT
+rm nvbit-Linux-x86_64-1.5.3.tar.bz2
 
 
