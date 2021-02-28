85ee0297ba77085c8df163142485e18012ba5f85
tgrogers <tgrogers@purdue.edu>
2021-02-28 11:16:47 -0500

    adding in a print for accel-sim committ too

--- util/plotting/merge-stats.py
+++ util/plotting/merge-stats.py
@@ -23,8 +23,10 @@ def get_csv_data_for_merge(filepath):
     configs = []
     cached_configs = []
     stats = []
-    build_num = None
-    build_nums = set()
+    gpgpu_build_num = None
+    gpgpu_build_nums = set()
+    accel_build_num = None
+    accel_build_nums = set()
     data = {}
     with open(filepath, 'r') as data_file:
         reader = csv.reader(data_file)        # define reader object
@@ -54,8 +56,11 @@ def get_csv_data_for_merge(filepath):
                                 appargs = appargs_kname[ :first_delimiter ]
                                 kname = appargs_kname[ first_delimiter + 2: ]
                                 if current_stat == "GPGPU-Sim-build" and data[config][count] != "NA":
-                                    build_num = data[config][count][21:28]
-                                    build_nums.add(build_num)
+                                    gpgpu_build_num = data[config][count][21:28]
+                                    gpgpu_build_nums.add(gpgpu_build_num)
+                                if current_stat == "Accel-Sim-build" and data[config][count] != "NA":
+                                    accel_build_num = data[config][count][16:23]
+                                    accel_build_nums.add(accel_build_num)
                                 stat_map[kname + appargs + config + current_stat ] = data[config][count]
                                 count += 1
                     apps = []
@@ -66,7 +71,7 @@ def get_csv_data_for_merge(filepath):
                     continue
                 else:
                     any_data = True
-                    full_config = row[0] + "-" + str(build_num)
+                    full_config = row[0] + "-accel-" + str(accel_build_num) + "-gpgpu-" + str(gpgpu_build_num)
                     configs.append(full_config)
                     data[full_config] = row[1:]
 
@@ -82,10 +87,11 @@ def get_csv_data_for_merge(filepath):
 
     # The assumption here is that every entry in each stats file is run with the same
     # git hash number, if not we are just going to warn and fail.
-    if len(build_nums) > 1:
-        exit("File {0} contains more than one build_num - this assumes one stats file has one build num: {1}"
-            .format(filepath, build_nums))
-    return all_named_kernels, stat_map, app_and_args, cached_configs, stats, build_nums
+    if len(gpgpu_build_nums) > 1 or len(accel_build_nums) > 1:
+        exit("File {0} contains more than one gpgpu_build_num or accelsim_build_num - this assumes one stats file has one build num: gpgpu: {1}" +\
+            " accel: {2}"
+            .format(filepath, gpgpu_build_nums, accel_build_nums))
+    return all_named_kernels, stat_map, app_and_args, cached_configs, stats, gpgpu_build_nums
 
 
 # After collection, spew out the tables
@@ -151,7 +157,10 @@ parser.add_option("-R", "--configs_as_rows", dest="configs_as_rows",
 
 csv_files = []
 for csvf in options.csv_files.split(","):
-    csv_files.append( common.file_option_test( csvf, "", this_directory ) )
+    try:
+        csv_files.append( common.file_option_test( csvf, "", this_directory ) )
+    except common.PathMissing as e:
+        print "Warning path {0}. Continuing".format(e)
 
 stats_per_file = {}
 for csvf in csv_files:
@@ -161,8 +170,9 @@ new_stats = {}
 new_configs = []
 union_apps_args = set()
 union_stats = set()
-for fname, ( all_named_kernels, stat_map, apps_and_args, configs, stats, build_nums ) in stats_per_file.iteritems():
-    print "Processing {0}".format(fname)
+for csvf in csv_files:
+    ( all_named_kernels, stat_map, apps_and_args, configs, stats, gpgpu_build_nums ) = stats_per_file[csvf]
+    print "Processing {0}".format(csvf)
     new_stats = dict(new_stats, **stat_map)
     new_configs += configs
 
@@ -175,10 +185,6 @@ for fname, ( all_named_kernels, stat_map, apps_and_args, configs, stats, build_n
         union_stats = set(stats)
     else:
         union_stats &= set(stats)
-
-print union_stats
-print union_apps_args
-print new_configs
 
 for stat in stats:
     print_stat( stat, all_named_kernels, apps_and_args, new_configs, new_stats, options.configs_as_rows )
