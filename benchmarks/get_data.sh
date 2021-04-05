ca4acb74652d521add63b37cb68309524f9bb688
Vijay Kandiah <vkz4947@peroni.cs.northwestern.edu>
2021-04-04 23:43:42 -0500

    fixing data_dirs for dct

--- benchmarks/get_data.sh
+++ benchmarks/get_data.sh
@@ -7,4 +7,6 @@ if [ ! -d $DATA_ROOT ]; then
     wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/benchmark_data/all.gpgpu-sim-app-data.tgz
     tar xzvf all.gpgpu-sim-app-data.tgz -C $BASH_ROOT
     rm all.gpgpu-sim-app-data.tgz
+	tar xzvf dct.tgz -C $DATA_ROOT
 fi
+
