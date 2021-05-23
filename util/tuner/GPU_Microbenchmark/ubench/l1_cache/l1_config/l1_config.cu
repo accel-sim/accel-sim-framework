8c142c76dcbde2080787bb6064011e035d87f5ae
Mahmoud Khairy A. Abdallah <abdallm@purdue.edu>
2021-05-23 12:19:18 -0400

    adding adaptive cache config to the tuner

--- util/tuner/GPU_Microbenchmark/ubench/l1_cache/l1_config/l1_config.cu
+++ util/tuner/GPU_Microbenchmark/ubench/l1_cache/l1_config/l1_config.cu
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 #include "../../../hw_def/hw_def.h"
@@ -15,13 +16,16 @@ TODO: we will automate this process
 // We know #sets from l1_assoc ubench (the l1 cache has 4 sets, since kepler
 // and in volta and turing)
 #define L1_CACHE_SETS 4
+
 // we know sector size from l1_assoc and l1_acces_grain ubenches and has
 // been consistent over generations, change it accordingly
 #define L1_SECTOR_SIZE 32
+
 // we know the mshr throughput from l1_mshr ubench
 // we find that each warp can issue up to two pending cache lines (8 sector
 // reqs)
-#define L1_MSHR_ENTRIES_PER_WARP 8
+#define L1_ACCESS_FACTOR L1_CACHE_LINE_SIZE/L1_SECTOR_SIZE
+#define L1_MSHR_ENTRIES_PER_WARP L1_ACCESS_FACTOR*2
 
 // L1 cache cache in Volta and above is write allocate, subsector write, write-
 // through we know that from l1_write_policy ubench and has been consistent
@@ -32,6 +36,9 @@ static const char *After_Volta_L1_Cache_Write_Policy = ",L:T:m:L:L,";
 // accsses to be write-back
 static const char *Before_Volta_L1_Cache_Write_Policy = ",L:L:m:N:L,";
 
+// Adaptive cache config option
+static const char *SHMEM_ADAPTIVE_OPTION = "0,8,16,32,64";
+
 int main() {
   intilizeDeviceProp(0);
 
@@ -39,42 +46,51 @@ int main() {
 
     std::cout << "\n//Accel_Sim config: \n";
 
-    unsigned MIN_L1_SIZE;
     bool adaptive_cache;
     string cache_write_string;
+    string adaptive_shmem_option_string;
+    unsigned write_cache_ratio;
     // l1 cache is sector since pascal
     char is_sector = (deviceProp.major >= 6) ? 'S' : 'N';
-    // for volta and above, l1 cache is adpative
+    // for volta and above, l1 is write allocate and adative
     if (deviceProp.major >= 7) {
       // configure based on min l1 cache
-      MIN_L1_SIZE = L1_SIZE - deviceProp.sharedMemPerMultiprocessor;
+      // l1 cache is adpative
       adaptive_cache = true;
+      adaptive_shmem_option_string = SHMEM_ADAPTIVE_OPTION;
+      std::stringstream large_shmem_size;
+      large_shmem_size << "," << deviceProp.sharedMemPerMultiprocessor/1024;
+      adaptive_shmem_option_string += large_shmem_size.str();
+      // set l1 write allocation policy (write allocate, write through)
       cache_write_string = After_Volta_L1_Cache_Write_Policy;
+      // L1 write-to-read ratio (25%) based on rodinia kmeans workload benchmarking
+      write_cache_ratio = 25;
     } else {
-      MIN_L1_SIZE = L1_SIZE;
       adaptive_cache = false;
       cache_write_string = Before_Volta_L1_Cache_Write_Policy;
+      write_cache_ratio = 0;
     }
 
     // lines per set
-    unsigned assoc = MIN_L1_SIZE / L1_CACHE_LINE_SIZE / L1_CACHE_SETS;
+    unsigned assoc = L1_SIZE / L1_CACHE_LINE_SIZE / L1_CACHE_SETS;
 
     unsigned warps_num_per_sm = MAX_THREADS_PER_SM / WARP_SIZE;
-    unsigned access_factor = L1_CACHE_LINE_SIZE / L1_SECTOR_SIZE;
     // each warp can issue up to two pending cache lines (this is based on our
     // l1_mshr ubench)
     unsigned mshr = warps_num_per_sm * L1_MSHR_ENTRIES_PER_WARP;
 
     std::cout << "-gpgpu_adaptive_cache_config " << adaptive_cache << std::endl;
+    std::cout << "-gpgpu_shmem_option " << adaptive_shmem_option_string << std::endl;
+    std::cout << "-gpgpu_unified_l1d_size " << L1_SIZE/1024 << std::endl;
     std::cout << "-gpgpu_l1_banks " << WARP_SCHEDS_PER_SM << std::endl;
-    // set old write settings for now as accel-sim does not support l1 write
-    // allocate yet
     std::cout << "-gpgpu_cache:dl1 " << is_sector << ":" << L1_CACHE_SETS << ":"
               << L1_CACHE_LINE_SIZE << ":" << assoc
-              << Before_Volta_L1_Cache_Write_Policy << "A:" << mshr << ":"
+              << cache_write_string << "A:" << mshr << ":"
               << warps_num_per_sm << ",16:0,32" << std::endl;
     std::cout << "-gpgpu_gmem_skip_L1D " << !deviceProp.globalL1CacheSupported
               << std::endl;
+    std::cout << "-gpgpu_l1_cache_write_ratio " << write_cache_ratio << std::endl;
+
   }
 
   return 1;
