461a662d612a39597e0b9876a0e75c577a0de557
VijayKandiah <vkz4947@peroni.cs.northwestern.edu>
2021-10-17 01:15:00 -0500

    Pulling dev branch of accel-sim/gpgpu-sim_distribution.git

--- gpu-simulator/setup_environment.sh
+++ gpu-simulator/setup_environment.sh
@@ -36,8 +36,11 @@ else
 fi
 
 if [ ! -d "$ACCELSIM_ROOT/gpgpu-sim" ] ; then
-    git clone https://github.com/gpgpu-sim/gpgpu-sim_distribution.git  $ACCELSIM_ROOT/gpgpu-sim
-    git -C $ACCELSIM_ROOT/gpgpu-sim/ checkout v4.0.1
+    git clone https://github.com/accel-sim/gpgpu-sim_distribution.git  $ACCELSIM_ROOT/gpgpu-sim
+    git -C $ACCELSIM_ROOT/gpgpu-sim/ checkout dev
+else
+    git -C $ACCELSIM_ROOT/gpgpu-sim/ checkout dev
+    git -C $ACCELSIM_ROOT/gpgpu-sim/ pull
 fi
 
 source $ACCELSIM_ROOT/gpgpu-sim/setup_environment $ACCELSIM_CONFIG
