3a6978eb8eba97032f14d85cabab7785927df7b3
Weili An <an107@purdue.edu>
2023-05-17 01:16:55 -0400

    Add F2FP opcode into ISA def files of ampere and turing

--- gpu-simulator/ISA_Def/ampere_opcode.h
+++ gpu-simulator/ISA_Def/ampere_opcode.h
@@ -87,6 +87,7 @@ static const std::unordered_map<std::string, OpcodeChar> Ampere_OpcodeMap = {
 
     // Conversion Instructions
     {"F2F", OpcodeChar(OP_F2F, ALU_OP)},
+    {"F2FP", OpcodeChar(OP_F2FP, ALU_OP)},
     {"F2I", OpcodeChar(OP_F2I, ALU_OP)},
     {"I2F", OpcodeChar(OP_I2F, ALU_OP)},
     {"I2I", OpcodeChar(OP_I2I, ALU_OP)},
