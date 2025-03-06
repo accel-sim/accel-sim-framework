a127221917d3c75c29ad6b01b28de33975d1bb16
christindbose <chris241@purdue.edu>
2025-03-06 16:17:13 -0500

    supporting code to run hopper traces

--- gpu-simulator/trace-driven/trace_driven.cc
+++ gpu-simulator/trace-driven/trace_driven.cc
@@ -41,6 +41,7 @@
 #include <vector>
 
 #include "../ISA_Def/accelwattch_component_mapping.h"
+#include "../ISA_Def/hopper_opcode.h"
 #include "../ISA_Def/ampere_opcode.h"
 #include "../ISA_Def/kepler_opcode.h"
 #include "../ISA_Def/pascal_opcode.h"
@@ -102,7 +103,9 @@ trace_kernel_info_t::trace_kernel_info_t(dim3 gridDim, dim3 blockDim,
   m_was_launched = false;
 
   // resolve the binary version
-  if (kernel_trace_info->binary_verion == AMPERE_RTX_BINART_VERSION ||
+  if (kernel_trace_info->binary_verion == HOPPER_H100_BINART_VERSION)
+    OpcodeMap = &Hopper_OpcodeMap;
+  else if (kernel_trace_info->binary_verion == AMPERE_RTX_BINART_VERSION ||
       kernel_trace_info->binary_verion == AMPERE_A100_BINART_VERSION)
     OpcodeMap = &Ampere_OpcodeMap;
   else if (kernel_trace_info->binary_verion == VOLTA_BINART_VERSION)
