01de2b088b64db38b19b82c829de89420b1ae14d
Ahmad <aalawneh@purdue.edu>
2025-04-24 11:23:41 +0000

    fix

--- util/hw_stats/run_hw.py
+++ util/hw_stats/run_hw.py
@@ -220,7 +220,7 @@ for bench in benchmarks:
 
                 extract_command = (
                     "ncu --import " + ncu_report_file +
-                    " --csv --page raw --target-processes all  " 
+                    " --csv --page raw   " 
                 )
                 profile_command = (
                     "ncu --metrics gpc__cycles_elapsed.avg,sm__cycles_elapsed.sum,smsp__inst_executed.sum,"
@@ -314,7 +314,7 @@ for bench in benchmarks:
 
                 extract_command = (
                     "ncu --import " + ncu_report_file +
-                    " --csv --page raw --target-processes all  " 
+                    " --csv --page raw  " 
                 )
                 sh_contents += (
                     '\nexport CUDA_VERSION="'
