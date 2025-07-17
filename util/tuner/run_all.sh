1577ebac12614db4351565ef36b838ccf3672842
Ahmad <aalawneh@purdue.edu>
2025-07-17 20:03:19 +0000

    push tuner rule

--- util/tuner/run_all.sh
+++ util/tuner/run_all.sh
@@ -3,7 +3,7 @@
 # THIS_DIR="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 SCRIPT_DIR="./gpu-app-collection-partial/src/cuda/GPU_Microbenchmark/"
 echo "Running make in $SCRIPT_DIR"
-make -C "$SCRIPT_DIR" -j || { echo "make failed"; exit 1; }
+make -C "$SCRIPT_DIR" tuner -j || { echo "make failed"; exit 1; }
 
 cd ${SCRIPT_DIR}/bin/
 for f in ./*; do
