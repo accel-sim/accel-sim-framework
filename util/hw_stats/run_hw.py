c1979dc31bdef43a77f2a5e446b8df97819e17f4
Weili An <an107@purdue.edu>
2025-09-01 16:21:49 -0400

    fix typo

--- util/hw_stats/run_hw.py
+++ util/hw_stats/run_hw.py
@@ -264,7 +264,7 @@ for bench in benchmarks:
                 
                 if options.set != "none":
                     sh_contents += (
-                        f"\nncu --set {options.set} -o {os.path.join(this_run_dir, f'ncu_set_{options.set}')};"
+                        f"\nncu --set {options.set} -o {os.path.join(this_run_dir, f'ncu_set_{options.set}')} {exec_path} {str(args)}; "
                     )
 
         for i in range(int(options.repeat_cycle)):
