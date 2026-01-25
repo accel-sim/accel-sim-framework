168484b1076dac0b6c81175be8dff7c9caa78141
JRPan <25518778+JRPan@users.noreply.github.com>
2026-01-24 19:51:46 -0500

    Add Hopper workflows and QV100/A100 support (#59)
    * +x
    
    * try again
    
    * test
    
    * rsync the folder
    
    * refactor
    
    * Enhance H200 trace generation script and update workflow to sync traces
    
    * Fix PATH export in H200 trace generation script
    
    * Update git clone URL in H200 trace generation script
    
    * Update H200 trace generation script to use GPU_Microbenchmark
    
    * Update H200 trace generation script to clone gpu-app-collection with submodules
    
    * Add creation of dummy data_dirs in H200 trace generation script
    
    * Move synced traces to trace depot in H200 workflow
    
    * add rodinia to hopper workflows
    
    * Clean up existing trace depot before moving new H200 traces
    
    * try hopper weekly
    
    * test if it work without actually running simulations
    
    * try again
    
    * checkout default branch
    
    * weekly ready. enable everything back
    
    * Add QV100 and A100

---
+++ .github/scripts/h200.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+echo "Starting H200 trace generation..."
+
+# Setup environment
+export CUDA_INSTALL_PATH=/usr/local/cuda-12.8/
+export PATH=$CUDA_INSTALL_PATH/bin:$PATH
+
+# Build Tracer
+echo "Building tracer..."
+./util/tracer_nvbit/install_nvbit.sh
+make clean -C ./util/tracer_nvbit/
+make -C ./util/tracer_nvbit/
+
+# Build applications
+echo "Building applications..."
+rm -rf ./gpu-app-collection/
+git clone --quiet --recurse-submodules https://github.com/accel-sim/gpu-app-collection.git
+source ./gpu-app-collection/src/setup_environment > /dev/null 2>&1
+make -C ./gpu-app-collection/src rodinia_2.0-ft rodinia-3.1 GPU_Microbenchmark
+# create dummy data_dirs
+ln -s /purdue/tgrghci/gpu-app-collection/data_dirs ./gpu-app-collection/data_dirs
+
+# Generate traces
+echo "Generating traces..."
+source ./gpu-app-collection/src/setup_environment
+rm -rf ./hw_run/
+./util/tracer_nvbit/run_hw_trace.py -B rodinia_2.0-ft,rodinia-3.1,GPU_Microbenchmark -D 7
+
+# Generate hardware stats
+echo "Generating hardware stats..."
+./util/hw_stats/run_hw.py -B rodinia_2.0-ft,rodinia-3.1,GPU_Microbenchmark -D 7
+echo "H200 trace generation completed!"
